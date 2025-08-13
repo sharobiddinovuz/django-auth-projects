1c1d192a7223e02bd84cb54d8eff60b1a3ac6588
Nurmuhammad <sharobiddinovnurmuhammad23@gmail.com>
2025-08-13 14:34:33 +0500

    added class based view functions

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import login_view, register_view, CustomLogoutView
+from .views import CustomLogoutView, UserCreateView, UserLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('accounts/register/', register_view, name="register"),
-    path('accounts/login/', login_view, name="login"),
+    path('accounts/register/', UserCreateView.as_view(), name="register"),
+    path('accounts/login/', UserLoginView.as_view(), name="login"),
     path('accounts/logout/', CustomLogoutView.as_view(), name="logout")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,48 +1,55 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LogoutView
 from .forms import UserRegisterForm, UserLoginForm
+from django.views.generic import CreateView, FormView
 from django.contrib import messages
 
 # Create your views here.
-def register_view(request):
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "registrations/register.html"
+    success_url = reverse_lazy("home")
 
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Muvaffaqiyatli ro'yhatdan o'tdingiz!")
-            return redirect(reverse_lazy('home'))
-        else:
-            messages.error(request, "Ro‘yxatdan o‘tishda xatolik bor. Ma’lumotlarni tekshiring.")
-    else:
-        form = UserRegisterForm()
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        messages.success(self.request, "Muvaffaqiyatli ro'yhatdan o'tdingiz!")
+        return response
 
-    return render(request, template_name='registrations/register.html', context={'form': form})
+    def form_invalid(self, form):
+        messages.error(self.request, "Ma'lumotlarni tekshirib qayta kiriting!")
+        return super().form_invalid(form)
 
-
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('home')
+class UserLoginView(FormView):
+    form_class = UserLoginForm
+    template_name = "registrations/login.html"
+    success_url = reverse_lazy('home')
     
-    if request.method=="POST":
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, "Xush kelibsiz!")
-                return redirect('home')
-            else:
-                messages.error(request, "Username yoki parol xato!")
-    else:
-        form = UserLoginForm()
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(self.get_success_url())
+        return super().get(request, *args, **kwargs)
 
-    return render(request, template_name='registrations/login.html', context={"form": form})
+    def form_valid(self, form): 
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            messages.success(self.request, "Xush kelibsiz!")
+            return super().form_valid(form)
+        else:
+            messages.error(self.request, "Username yoki parol xato!")
+            return super().form_invalid(form)
+    
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Formada xatolik bor qayta tekshirib ko'ring!")
+        return super().form_invalid(form)
 
 
 class CustomLogoutView(LogoutView):
